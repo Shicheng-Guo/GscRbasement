e261dbb850461a6f697a2d8f7017d9c1b56ae09f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-07-08 16:35:41 -0700

    Create CvSampling.R

---
+++ CvSampling.R
@@ -0,0 +1,15 @@
+CvSampling<- function(Nobs=1000,K=5){
+  #======================
+  # return sample row number: used to sampling the subset to cross-validation predition.
+  # rank<-CvSampling(Nobs=dim(data)[1],K=5)
+  # train=x[[1]]$train
+  # test=x[[1]]$test
+  #====================
+  rs <- runif(Nobs)
+  id <- seq(Nobs)[order(rs)]
+  k <- as.integer(Nobs*seq(1,K-1)/K)
+  k <- matrix(c(0,rep(k,each=2),Nobs),ncol=2,byrow=TRUE)
+  k[,1] <- k[,1]+1
+  l <- lapply(seq.int(K),function(x,k,d) list(train=d[!(seq(d) %in% seq(k[x,1],k[x,2]))], test=d[seq(k[x,1],k[x,2])]),k=k,d=id)
+  return(l)
+}

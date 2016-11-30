03011f514211b2e94b79c3e0fd2e58e42e6b9d62
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-29 22:03:19 -0800

    Create write.tbed.R

---
+++ write.tbed.R
@@ -0,0 +1,10 @@
+write.bed<-function(bed,file,extend=0){
+  bed[,2]<-as.numeric(as.character(bed[,2]))-extend
+  bed[,3]<-as.numeric(as.character(bed[,3]))+extend
+  if(ncol(bed)==3){
+  bed[,4]<-paste(bed[,1],":",bed[,2],"-",bed[,3],sep="")  
+  }
+  if(ncol(bed)>=4){
+    write.table(bed,file=file,sep="\t",col.names=F,row.names=F,quote=F)
+  }
+}

810b67d1e936bb92e44244acbdca181564909e7f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-02-24 00:00:35 -0800

    Update cor2bed.R

--- cor2bed.R
+++ cor2bed.R
@@ -1,4 +1,5 @@
 cor2bed<-function(cor){
+  cor<-as.character(cor)
   a<-unlist(lapply(strsplit(cor,split=c(":")),function(x) strsplit(x,"-")))
   bed<-matrix(a,ncol=3,byrow=T)
   return(data.frame(bed))

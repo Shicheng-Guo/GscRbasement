8cf85ffd50f4d678f89d0eab2dc86c864d8e36fb
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-04-26 16:43:37 -0500

    Update ReactomePLOT.R

--- ReactomePLOT.R
+++ ReactomePLOT.R
@@ -1,10 +1,12 @@
-ReactomePLOT<-function(symbols){
+ReactomePLOT<-function(symbols,output){
 library("ReactomePA")
 library("org.Hs.eg.db")
 ENTREZID<-mapIds(org.Hs.eg.db, symbols, 'ENTREZID', 'SYMBOL')
 x <- enrichPathway(gene=ENTREZID,pvalueCutoff=0.05, readable=T)
-barplot(x, showCategory=30)
-dotplot(x, showCategory=30)
-emapplot(x)
-cnetplot(x, categorySize="pvalue", foldChange=geneList)
+P1<-barplot(x, showCategory=30)
+P2<-dotplot(x, showCategory=30)
+P3<-emapplot(x)
+ggsave(P1,file=paste(output,"reactome.barplot.pdf",sep="."))
+ggsave(P2,file=paste(output,"reactome.dotplot.pdf",sep="."))
+ggsave(P3,file=paste(output,"reactome.emapplot.pdf",sep="."))
 }

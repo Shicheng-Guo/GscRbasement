3de6e849d856936d29d0e8cd6ba30d3b5585f021
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-04-26 17:32:26 -0500

    Update ReactomePLOT.R

--- ReactomePLOT.R
+++ ReactomePLOT.R
@@ -1,12 +1,14 @@
 ReactomePLOT<-function(symbols,output){
-library("ReactomePA")
-library("org.Hs.eg.db")
-ENTREZID<-mapIds(org.Hs.eg.db, symbols, 'ENTREZID', 'SYMBOL')
-x <- enrichPathway(gene=ENTREZID,pvalueCutoff=0.05, readable=T)
-P1<-barplot(x, showCategory=30)
-P2<-dotplot(x, showCategory=30)
-P3<-emapplot(x)
-ggsave(P1,file=paste(output,"reactome.barplot.pdf",sep="."))
-ggsave(P2,file=paste(output,"reactome.dotplot.pdf",sep="."))
-ggsave(P3,file=paste(output,"reactome.emapplot.pdf",sep="."))
+  library("ReactomePA")
+  library("org.Hs.eg.db")
+  ENTREZID<-mapIds(org.Hs.eg.db, symbols, 'ENTREZID', 'SYMBOL')
+  x <- enrichPathway(gene=ENTREZID,pvalueCutoff=0.05, readable=T)
+  if(nrow(as.data.frame(x))>0){
+  P1<-barplot(x, showCategory=30)
+  P2<-dotplot(x, showCategory=30)
+  P3<-emapplot(x)
+  ggsave(P1,file=paste(output,"reactome.barplot.pdf",sep="."))
+  ggsave(P2,file=paste(output,"reactome.dotplot.pdf",sep="."))
+  ggsave(P3,file=paste(output,"reactome.emapplot.pdf",sep="."))
+}
 }

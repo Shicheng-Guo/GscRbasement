b05cb858a7c187d42e5fbc1ad71fe27811323773
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-05-30 13:43:09 -0500

    Update GscTools.R

--- GscTools.R
+++ GscTools.R
@@ -1,5 +1,17 @@
 library("Haplin")
 
+manifest2barcode<-function(manifest){
+x=read.table(manifest,header = T)
+manifest_length= nrow(x)
+id= toString(sprintf('"%s"', x$id))
+Part1= '{"filters":{"op":"in","content":{"field":"files.file_id","value":[ '
+Part2= '] }},"format":"TSV","fields":"file_id,file_name,cases.submitter_id,cases.case_id,data_category,data_type,cases.samples.tumor_descriptor,cases.samples.tissue_type,cases.samples.sample_type,cases.samples.submitter_id,cases.samples.sample_id,cases.samples.portions.analytes.aliquots.aliquot_id,cases.samples.portions.analytes.aliquots.submitter_id","size":'
+Part3= paste0("\"",manifest_length, "\"", "}")
+Sentence= paste(Part1,id,Part2,Part3, collapse=" ")
+write.table(Sentence,"Payload.txt",quote=F,col.names=F,row.names=F)
+system("curl --request POST --header \"Content-Type: application/json\" --data @Payload.txt \"https://api.gdc.cancer.gov/files\" > barcode.txt")
+}
+
 ENST2Symbol<-function(ENST){
   db<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/AnnotationDatabase/master/ENSG.ENST.ENSP.Symbol.hg19.bed",sep="\t")
   Symbol<-db[match(ENST,db$V7),4]

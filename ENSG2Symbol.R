392b7ca8d38bbf291572a7f2fc334def769e499d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-06-27 13:37:43 -0500

    Update ENSG2Symbol.R

--- ENSG2Symbol.R
+++ ENSG2Symbol.R
@@ -1,5 +1,6 @@
 ENSG2Symbol<-function(ENSG){
   db<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/AnnotationDatabase/master/ENSG.ENST.ENSP.Symbol.hg19.bed",sep="\t")
-  Symbol<-db[match(ENSG,db$V8),4]
+  ENSG<-unlist(lapply(strsplit(ENSG,split="[.]"),function(x) x[1]))
+  Symbol<-db[match(as.character(ENSG),db$V8),4]
   return(Symbol)
 }

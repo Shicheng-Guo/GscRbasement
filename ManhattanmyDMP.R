b1a8a111c5af96b20f8e910eb79f2d0da9d969b4
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-13 20:21:01 -0500

    Create ManhattanmyDMP.R

---
+++ ManhattanmyDMP.R
@@ -0,0 +1,18 @@
+ManhattanmyDMP<-function(myDMP){
+  library(qqman)
+  SNP=rownames(myDMP)
+  CHR=myDMP$CHR
+  if(length(grep("X",CHR))>0){
+    CHR<-sapply(CHR,function(x) gsub(pattern = "X",replacement = "23",x))
+    CHR<-sapply(CHR,function(x) gsub(pattern = "Y",replacement = "24",x))
+  }
+  CHR<-as.numeric(CHR)
+  BP=myDMP$MAPINFO
+  P=myDMP$P.Value
+  manhattaninput=data.frame(SNP,CHR,BP,P)
+  max<-max(2-log(manhattaninput$P,10))
+  genomewideline=0.05/nrow(manhattaninput)
+  pdf("manhattan.pdf")
+  manhattan(manhattaninput,col = c("blue4", "orange3"),ylim = c(0,10),genomewideline=min(subset(myDMP,adj.P.Val<0.05)$P.Value),lwd=2, suggestiveline=F)
+  dev.off()
+}

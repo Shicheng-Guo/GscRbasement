86991d754feb60e6f9928d45fe74432e73d6d77b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-22 22:40:29 -0500

    Create radiation.clinical.TCGA.R

---
+++ radiation.clinical.TCGA.R
@@ -0,0 +1,25 @@
+radiation2csv<-function(clinical.radiation){
+  bcr_patient_barcode<-clinical.radiation$bcr_patient_barcode
+  radiation_type<-clinical.radiation$radiation_type
+  radiation_dosage<-clinical.radiation$radiation_dosage
+  radiation_treatment_ongoing<-clinical.radiation$radiation_treatment_ongoing
+  measure_of_response<-clinical.radiation$measure_of_response
+  days_to_radiation_therapy_start<-clinical.radiation$days_to_radiation_therapy_start
+  days_to_radiation_therapy_end<-clinical.radiation$days_to_radiation_therapy_end
+  measure_of_response<-clinical.radiation$measure_of_response
+  new.clinical.radiation<-data.frame(bcr_patient_barcode,radiation_type,radiation_dosage,radiation_treatment_ongoing,days_to_radiation_therapy_start,days_to_radiation_therapy_end,measure_of_response)
+  return(new.clinical.radiation)
+}
+
+setwd("~/hpc/project/TCGA")
+library("TCGAbiolinks")
+pid<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/HowtoBook/master/TCGA/drug_response/Pid.drugResponse.txt",head=F,sep="\t")
+Radiation<-c()
+for(i in 1:nrow(pid)){
+query <- GDCquery(project =pid[i,1],data.category = "Clinical", file.type = "xml")
+GDCdownload(query)
+clinical.radiation <- GDCprepare_clinic(query, clinical.info = "radiation")
+radiation<-unique(radiation2csv(clinical.radiation))
+Radiation<-rbind(Radiation,radiation)
+}
+write.table(Radiation,file="Radiation.txt",sep="\t",col.names = NA,row.names = T,quote=F)

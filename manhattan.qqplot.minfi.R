05f046d3deea0c75904de525c534a22d9f9b93b9
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-09-16 17:40:10 -0500

    Update manhattan.qqplot.minfi.R

--- manhattan.qqplot.minfi.R
+++ manhattan.qqplot.minfi.R
@@ -1,21 +1,20 @@
 library("qqman")
 library("Haplin")
 args = commandArgs(trailingOnly=TRUE)
-mylimma<-read.table(args,head=T,sep="",check.names=F)
+mylimma<-read.table(args,head=T,sep="\t",check.names=F)
 mylimma=data.frame(CHR=mylimma$CHR,SNP=mylimma$ID,BP=mylimma$MAPINFO,P=mylimma$pval)
 seed=sample(seq(1,100000,by=1),1)
 manhattan.plot<-function(mylimma){
 CHR=mylimma$CHR
-if(length(grep("X",CHR))>0){
-  CHR<-sapply(CHR,function(x) gsub(pattern = "X",replacement = "23",x))
-  CHR<-sapply(CHR,function(x) gsub(pattern = "Y",replacement = "24",x))
+if(length(grep("X",mylimma$CHR))>0){
+  mylimma$CHR<-sapply(mylimma$CHR,function(x) gsub(pattern = "X",replacement = "23",x))
+  mylimma$CHR<-sapply(mylimma$CHR,function(x) gsub(pattern = "Y",replacement = "24",x))
 }
-CHR<-as.numeric(CHR)
-manhattaninput=data.frame(SNP=mylimma$SNP,CHR=CHR,BP=mylimma$BP,P=mylimma$P)
-max<-max(2-log(manhattaninput$P,10))
-genomewideline=0.05/nrow(manhattaninput)
+mylimma$CHR<-as.numeric(mylimma$CHR)
+max<-max(2-log(mylimma$P,10))
+genomewideline=0.05/nrow(mylimma)
 pdf(paste("manhattan.",seed,".pdf",sep=""))
-manhattan(manhattaninput,col = c("blue4", "orange3"),ylim = c(0,10),genomewideline=-log10(genomewideline),lwd=2, suggestiveline=F)
+manhattan(mylimma,col = c("blue4", "orange3"),ylim = c(0,10),genomewideline=-log10(genomewideline),lwd=2, suggestiveline=F)
 dev.off()
 }
 manhattan.plot(mylimma)

170c4c3dc8116a776c8e17cd6ce93c29a8f3cf6f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-24 18:07:46 -0500

    Update qqplot.R

--- qqplot.R
+++ qqplot.R
@@ -1,4 +1,10 @@
-qqplot<-function(pvalues,output="qqplot.pdf"){
+args = commandArgs(trailingOnly=TRUE)
+input=args[1]
+Pcol=args[2]
+output=args[3]
+P=read.table(input)
+pvalues=as.numeric(P[,Pcol])
+qqplot<-function(pvalues,output=output){
 library("Haplin")
 pdf(output)
   pQQ(pvalues, nlabs =length(pvalues), conf = 0.95) 

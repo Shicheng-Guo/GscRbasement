3e152cfa22d5160ac064572408052676606c1556
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-24 18:09:14 -0500

    Update qqplot.R

--- qqplot.R
+++ qqplot.R
@@ -4,9 +4,12 @@ Pcol=args[2]
 output=args[3]
 P=read.table(input)
 pvalues=as.numeric(P[,Pcol])
-qqplot<-function(pvalues,output=output){
+qqplot<-function(pvalues,output=paste(output,".pdf",sep=""){
 library("Haplin")
 pdf(output)
   pQQ(pvalues, nlabs =length(pvalues), conf = 0.95) 
 dev.off()
 }
+# wget https://raw.githubusercontent.com/Shicheng-Guo/ASA/master/manhattan.plot.R -O manhattan.plot.R
+# Rscript manhattan.plot.R Schrodi_IL23_IL17.Kbac.assoc 4 Schrodi_IL23_IL17.Kbac.assoc
+

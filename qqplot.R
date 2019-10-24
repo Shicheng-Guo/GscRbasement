6284f33625e3cbcfed176b0bcdb87483d9685379
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-10-24 18:44:34 -0500

    Update qqplot.R

--- qqplot.R
+++ qqplot.R
@@ -1,14 +1,13 @@
 args = commandArgs(trailingOnly=TRUE)
-input=args[1]
-Pcol=args[2]
-P=read.table(input)
-pvalues=as.numeric(P[,Pcol])
-qqplot<-function(pvalues,output=paste(input,"qqplot.pdf",sep=""){
 library("Haplin")
+input=args[1]
+Pcol=as.numeric(args[2])
+data=read.table(input,head=T,sep="")
+pvalues=as.numeric(data[,Pcol])
+qqplot<-function(pvalues,output=paste(input,"qqplot.pdf",sep="")){
 pdf(output)
-  pQQ(pvalues, nlabs =length(pvalues), conf = 0.95) 
+  pQQ(pvalues, nlabs =length(pvalues), conf = 0.95)
 dev.off()
 }
 # wget https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/qqplot.R -O qqplot.R
 # Rscript manhattan.plot.R Schrodi_IL23_IL17.Kbac.assoc 4 Schrodi_IL23_IL17.Kbac.assoc
-

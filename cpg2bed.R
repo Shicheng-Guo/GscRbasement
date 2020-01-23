8df35a53b4f8a650f4d2a635369132b5f4637476
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-22 20:36:34 -0600

    Update cpg2bed.R

--- cpg2bed.R
+++ cpg2bed.R
@@ -1,4 +1,4 @@
-cpg2symbol<-function(cpg){
+cpg2bed<-function(cpg){
 map<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/AnnotationDatabase/master/hg19/GPL13534_450K_hg19_V3.bed")
 bed<-map[match(cpg,map[,4]),]
 return(bed)

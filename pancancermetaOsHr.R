a3c58e9d9b7b13f722dd86973fb46cb2a464a840
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-22 20:51:36 -0600

    Update pancancermetaOsHr.R

--- pancancermetaOsHr.R
+++ pancancermetaOsHr.R
@@ -1,45 +1,113 @@
-OROSmerge<-function(memo){
-file<-list.files(pattern="*pvalue.csv")
-dge<-read.csv(file[1])
-os<-read.csv(file[2])
-out<-merge(dge,os,by="X")
+pancancermetaOsHr<-function(symbolist,memo){
+  source("https://raw.githubusercontent.com/Shicheng-Guo/GscRbasement/master/GscTools.R")
+  library("meta")
+  library("metafor")
+  library("survival")
+  library("survminer")
 
-pick<-subset(out,pval<10^-4 & pval.fixed<0.05 & beta*(TE.fixed-1)>0)
+  db<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/AnnotationDatabase/master/ENSG.ENST.ENSP.Symbol.hg19.bed",sep="\t")
+  Symbol2ENSG<-function(Symbol,db){
+    ENSG<-as.character(db[match(Symbol,db$V4),8])
+    ENSG<-na.omit(data.frame(Symbol,ENSG))
+    return(ENSG)
+  }
+  ENSG2Symbol<-function(ENSG,db){
+    ENSG<-unlist(lapply(strsplit(ENSG,split="[.]"),function(x) x[1]))
+    Symbol<-db[match(as.character(ENSG),db$V8),4]
+    return(Symbol)
+  }
+  ensg2bed<-function(ENSG,db){
+    ENSG<-unlist(lapply(strsplit(ENSG,split="[.]"),function(x) x[1]))
+    bed<-unique(db[db$V5 %in% as.character(ENSG),c(1,2,3,5)])
+    return(bed)
+  }
 
-dir.create("pick")
-# be careful to run it or else it will takes quite long time
-for(i in 1:nrow(pick)){
-x<-paste("cp *-",pick[i,1],"* ./pick",sep="")
-system(x)
-}
-# install.packages("CMplot")
-setwd("pick")
-system("cp ../*.csv ./")
-library("CMplot")
-ensg<-read.table("~/hpc/db/hg19/ENSG.hg19.bed")
-ensgrlt<-ensg[match(unlist(lapply(strsplit(as.character(out$X),"[.]"),function(x) x[1])),ensg[,5]),]
-output<-data.frame(ensgrlt,out)
+  chr2num<-function(x){
+    x<-output$V1
+    x<-gsub("chr","",x)
+    x[x=="X"]<-23
+    x[x=="Y"]<-24
+    return(x)
+  }
 
-chr2num<-function(x){
-x<-output$V1
-x<-gsub("chr","",x)
-x[x=="X"]<-23
-x[x=="Y"]<-24
-return(x)
-}
 
-write.csv(out,file=paste(memo,".tcga.pancancer.overall.rnaseq.dmg.smd",".os.hr.csv",sep=""),quote=F)
-write.csv(pick,file=paste(memo,".tcga.pancancer.pick.rnaseq.dmg.smd","os.hr.csv",sep=""),quote=F)
+  load("~/hpc/methylation/Pancancer/RNA-seq/rnaseqdata.pancancer.RData")
 
-cminput<-na.omit(data.frame(SNP=output[,ncol(output)],Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval,stringsAsFactors=F))
-CMplot(cminput,plot.type="b",memo=paste(memo,".dge.fix",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
-write.csv(cminput[order(cminput[,ncol(cminput)]),],file=paste(memo,".pval.manhattan.qqplot.meta.dge.bed",sep=""),quote=F,row.names=F)
+  TCGAProjects=c("BLCA","BRCA","CESC","CHOL","COAD","ESCA","GBM","HNSC","KICH","KIRC","KIRP","LIHC","LUAD","LUSC","PAAD","PCPG","PRAD","READ","SARC","STAD","THCA","THYM","UCEC")
+  panc<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/PANC/master/extdata/panc.txt",head=T)
+  phen1=read.table("https://raw.githubusercontent.com/Shicheng-Guo/PANC/master/extdata/TCGA-clinical-11093.tsv",header = T,sep="\t")
+  phen2=read.table("https://raw.githubusercontent.com/Shicheng-Guo/PANC/master/extdata/File_metadata2.txt",header = T,sep="\t")
+  head(phen1)
+  head(phen2)
+  colnames(rnaseqdata)<-unlist(lapply(strsplit(colnames(rnaseqdata),"/"),function(x) x[2]))
+  phen<-data.frame(phen2,phen1[match(phen2$cases.0.case_id,phen1$case_id),])
+  phen$file_name=gsub(".gz","",phen$file_name)
+  # prepare phenotype information
+  phen<-phen[match(colnames(rnaseqdata),phen$file_name),]
+  phen$phen4<-id2phen4(phen$cases.0.samples.0.submitter_id)
+  phen$phen3<-id2phen3(phen$cases.0.samples.0.submitter_id)
+  phen$phen2<-id2bin(phen$cases.0.samples.0.submitter_id)
+  phen$pid<-phen$project_id
+  head(phen)
 
-cminput<-na.omit(data.frame(SNP=output[,ncol(output)],Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.fix))
-CMplot(cminput,plot.type="b",memo=paste(memo,".fix",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
-write.csv(cminput[order(cminput[,ncol(cminput)]),],file=paste(memo,".pval.fix.manhattan.qqplot.meta.OS.HR.bed",sep=""),quote=F,row.names=F)
+  OS<-read.table("https://raw.githubusercontent.com/Shicheng-Guo/HowtoBook/master/TCGA/OverallSurvivalTime.txt",head=T,sep="\t")
+  # match survival information
+  idx<-which(c(phen$phen2==1))
+  phen<-phen[idx,]
+  input<-rnaseqdata[,idx]
+  input<-RawZeroRemove(input)
+  idx<-na.omit(match(OS$submitter_id,phen$phen3))
+  input<-log(input[,idx]+1,2)
 
-cminput<-na.omit(data.frame(SNP=output[,ncol(output)],Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.random))
-CMplot(cminput,plot.type="b",memo=paste(memo,".random",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
-write.csv(cminput[order(cminput[,ncol(cminput)]),],file=paste(memo,".pval.random.manhattan.qqplot.meta.OS.HR.bed",sep=""),quote=F,row.names=F)
-}
+  phen<-phen[idx,]
+  phen<-data.frame(phen,OS[match(phen$phen3,OS$submitter_id),])
+  phen$censored<-as.numeric(!phen$censored)
+  phen$week=phen$time/7
+
+  ENSG<-Symbol2ENSG(as.character(symbolist),db)
+  xgene<-c(as.character(ENSG[,2]))
+  ii<-na.omit(unique(unlist(lapply(xgene,function(x) grep(x,rownames(input))))))
+  out<-c()
+  z<-1                                   
+  for(i in ii){
+    HR<-c()
+    z<-z+1
+    for(TCGAProject in TCGAProjects){
+      newdata<-input[,phen$project_id==paste("TCGA-",TCGAProject,sep="")]
+      xphen<-phen[phen$project_id==paste("TCGA-",TCGAProject,sep=""),]
+      dat<-data.frame(Rna=newdata[i,],xphen)
+      thres<-mean(dat[,1],na.rm=T)
+      hr.fit<-summary(coxph(Surv(week,censored)~Rna,dat))
+      hr1=hr.fit$coefficients[1,]
+      hr2=hr.fit$conf.int[1,]
+      HR<-rbind(HR,c(hr1,hr2[3],hr2[4]))
+    }
+
+    rownames(HR)<-TCGAProjects
+    m<-metagen(HR[,1],seTE=HR[,3],comb.fixed = TRUE,comb.random = TRUE,prediction=F,sm="HR")
+    fixedEffect<-c(exp(m$TE.fixed),exp(m$lower.fixed),exp(m$upper.fixed),m$pval.fixed)
+    randomEffect<-c(exp(m$TE.random),exp(m$lower.random),exp(m$upper.random),m$pval.random)
+    out<-rbind(out,c(fixedEffect,randomEffect))
+    print(c(z,i,rownames(input)[i]))
+    pdf(paste(ENSG2Symbol(rownames(input)[i],db),"-",rownames(input)[i],".OS.HR.PANC.pdf",sep=""))
+    forest(m,leftlabs = rownames(HR),
+           lab.e = "Intervention",
+           pooled.totals = FALSE,
+           smlab = "",studlab=rownames(HR),
+           text.random = "Overall effect",
+           print.tau2 = FALSE,
+           col.diamond = "blue",
+           col.diamond.lines = "black",
+           col.predict = "red",
+           print.I2.ci = TRUE,
+           digits.sd = 2,fontsize=9,xlim=c(0.5,2))
+    dev.off()
+    write.table(HR,file=paste(ENSG2Symbol(rownames(input)[i],db),"-",rownames(input)[i],".OS.HR.EACH.txt",sep=""),sep="\t",quote=F,col.names=NA,row.names=T)
+  }
+colnames(out)<-c("TE.fixed","lower.fixed","upper.fixed","pval.fixed","TE.random","lower.random","upper.random","pval.random")
+rownames(out)<-rownames(input)[ii]
+out3<-data.frame(out)
+out3<-out3[order(out3$pval.random),]
+out3$symbol<-as.character(ENSG2Symbol(as.character(rownames(out3)),db))
+write.table(out3,file=paste(memo,"tcga.pancancer.meta.pvalue.txt",sep=""),sep="\t",quote=F,col.names = NA,row.names = T)
+write.csv(out3,file=paste(memo,"tcga.pancancer.meta.pvalue.csv",sep=""),quote=F)

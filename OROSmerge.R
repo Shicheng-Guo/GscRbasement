67f739b4e862ded03c782ffe36b6b3f10e9e4684
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-22 19:52:27 -0600

    Update OROSmerge.R

--- OROSmerge.R
+++ OROSmerge.R
@@ -1,10 +1,16 @@
 OROSmerge<-function(memo){
+memo="housekeeping"
 file<-list.files(pattern="*pvalue.csv")
 dge<-read.csv(file[1])
 os<-read.csv(file[2])
 out<-merge(dge,os,by="X")
 
 pick<-subset(out,pval<10^-4 & pval.fixed<0.05 & beta*(TE.fixed-1)>0)
+pick<-pick[order(pick$pval),]
+
+if(nrow(pick)>80){
+pick<-pick[80,]
+}
 
 dir.create("pick")
 # be careful to run it or else it will takes quite long time
@@ -28,20 +34,18 @@ x[x=="Y"]<-24
 return(x)
 }
 
-write.csv(out,file=paste(memo,".tcga.pancancer.chol.overall.rnaseq.dmg.smd",".os.hr.csv",sep=""),quote=F)
-write.table(out,file=paste(memo,".tcga.pancancer.chol.overall.rnaseq.dmg.smd",".os.hr.txt",sep=""),quote=F,col.names=NA,row.names=T,sep="\t")
-write.csv(pick,file=paste(memo,".tcga.pancancer.chol.pick.rnaseq.dmg.smd","os.hr.csv",sep=""),quote=F)
-write.table(pick,file=paste(memo,".tcga.pancancer.chol.pick.rnaseq.dmg.smd","os.hr.txt",sep=""),quote=F,col.names=NA,row.names=T,sep="\t")
+write.csv(out,file=paste(memo,".tcga.pancancer.overall.rnaseq.dmg.smd",".os.hr.csv",sep=""),quote=F)
+write.csv(pick,file=paste(memo,".tcga.pancancer.pick.rnaseq.dmg.smd","os.hr.csv",sep=""),quote=F)
 
-cminput<-na.omit(data.frame(SNP=output$V5,Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval,stringsAsFactors=F))
+cminput<-na.omit(data.frame(SNP=output[,ncol(output)],Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval,stringsAsFactors=F))
 CMplot(cminput,plot.type="b",memo=paste(memo,".dge.fix",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
-write.table(cminput,file=paste(memo,".pval.manhattan.qqplot.meta.dge.txt",sep=""),sep="\t",quote=F,row.name=T,col.names=NA)
+write.csv(cminput[order(cminput[,ncol(cminput)]),],file=paste(memo,".pval.manhattan.qqplot.meta.dge.bed",sep=""),quote=F,row.names=F)
 
-cminput<-na.omit(data.frame(SNP=output$V5,Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.fix))
+cminput<-na.omit(data.frame(SNP=output[,ncol(output)],Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.fix))
 CMplot(cminput,plot.type="b",memo=paste(memo,".fix",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
-write.table(cminput,file=paste(memo,".HR.OS.pval.fix.manhattan.qqplot.meta.OS.HR.txt",sep=""),sep="\t",quote=F,row.name=T,col.names=NA)
+write.csv(cminput[order(cminput[,ncol(cminput)]),],file=paste(memo,".pval.fix.manhattan.qqplot.meta.OS.HR.bed",sep=""),quote=F,row.names=F)
 
-cminput<-na.omit(data.frame(SNP=output$V5,Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.random))
+cminput<-na.omit(data.frame(SNP=output[,ncol(output)],Chromosome=chr2num(output$V1),Position=output$V2,trait1=output$pval.random))
 CMplot(cminput,plot.type="b",memo=paste(memo,".random",sep=""),LOG10=TRUE,threshold=NULL,file="jpg",dpi=300,file.output=TRUE,verbose=TRUE,width=14,height=6)
-write.table(cminput,file=paste(memo,".HR.OS.pval.random.manhattan.qqplot.meta.OS.HR.txt",sep=""),sep="\t",quote=F,row.name=T,col.names=NA)
+write.csv(cminput[order(cminput[,ncol(cminput)]),],file=paste(memo,".pval.random.manhattan.qqplot.meta.OS.HR.bed",sep=""),quote=F,row.names=F)
 }
